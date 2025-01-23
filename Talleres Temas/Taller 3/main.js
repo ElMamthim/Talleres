bd50b8122a8fc5e147ad609aaade6d36d50c009b
ElMamthim <mathiaschef23@gmail.com>
2025-01-22 21:41:24 -0500

    [+] Avances del punto 3 y otras cosas realizadas en clase 22/01/25

--- Talleres Temas/Taller 3/main.js
+++ Talleres Temas/Taller 3/main.js
@@ -2,6 +2,7 @@
 
 function sumar(lista) {
     let suma = 0;
+    
     for (let i = 0; i < lista.length; i++) {
         suma += lista[i];
     }
@@ -13,11 +14,12 @@ console.log('Los elementos sumados dan', sumar(numeros));
 
 // 2. Encontrar el número máximo: Dada una lista de números, crea una función en JavaScript que recorra la lista y encuentre el número máximo en la lista. Luego, devuelve ese número.  
 
-function nummax(list) {
-    let max = list[0];
-    for (let i = 1; i < list.length; i++) {
-        if (list[i] > max) {
-            max = list[i];
+function nummax(lista) {
+    let max = lista[0];
+
+    for (let i = 1; i < lista.length; i++) {
+        if (lista[i] > max) {
+            max = lista[i];
         }
     }
     return max;
@@ -30,6 +32,7 @@ console.log('El numero maximo es', nummax(num));
 
 function pares(lista) {
     let paresn = [];
+
     for (let i = 0; i < lista.length; i++) {
         if (lista[i] % 2 == 0) {
             paresn.push(lista[i]);
@@ -49,12 +52,78 @@ console.log('Los numeros pares son', paresf(pars));
 
 // 4. Contar elementos repetidos: Dada una lista de elementos, crea una función en JavaScript que recorra la lista y cuente la cantidad de veces que aparece cada elemento. Devuelve un objeto donde las claves sean los elementos de la lista y los valores sean la cantidad de veces que aparecen.
 
-function contar() {
+function contar(lista) {
     const conteo = {};
     
+    for (let i = 0; i < lista.length; i++) {
+        const elemento = lista[i];
+        if (conteo[elemento]) {
+            conteo[elemento]++;
+        } 
+        else {
+            conteo[elemento] = 1;
+        }
+    }
+    return conteo;
 }
 
 const elementos = ['manzana', 'pera', 'manzana', 'uva', 'pera', 'manzana', 'plátano'];
-console.log('se encontraron los siguientes elementos', contar(elementos));
+console.log('Los elementos encontrados son', contar(elementos));
 
 // 5. Encontrar la cadena más larga: Dada una lista de cadenas, crea una función en JavaScript que recorra la lista y encuentre la cadena más larga. Devuelve esa cadena.
+
+function cadenalarga(lista) {
+    let cadenamlarga = ''; 
+
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].length > cadenamlarga.length) {
+            cadenamlarga = lista[i];
+        }
+    }
+    return cadenamlarga;
+}
+
+const cadenas = ['Mathias', 'Lucas', 'Juan', 'Sebastian', 'Adriano', 'Erica', 'Laura', 'Sara'];
+console.log('La cadena mas larga es', cadenalarga(cadenas));
+
+// 6. Concatenar cadenas: Dada una lista de cadenas, crea una función en JavaScript que recorra la lista y concatene todas las cadenas en una sola. Devuelve la cadena resultante.
+
+function concatenacion(lista) {
+    let cadenaconcatenada = "";
+
+    for (let i = 0; i < lista.length; i++) {
+        cadenaconcatenada += lista[i];
+    }
+    return cadenaconcatenada;
+}
+
+const cadenasc = ['Hola', ' ', 'como', ' ', 'estan', ' ', 'todos!' ];
+console.log('La concatenacion es', concatenacion(cadenasc));
+
+// 7. Ordenar lista de números: Dada una lista de números desordenados, crea una función en JavaScript que recorra la lista y la ordene de menor a mayor. Devuelve la lista ordenada.
+
+function listan(lista) {
+    for (let i = 0; i < lista.length; i++) { 
+        for (let j = 0; j < lista.length - 1; j++) { 
+            if (lista[j] > lista[j + 1]) { 
+                let list = lista[j];
+                lista[j] = lista[j + 1]; //Let adicional para que se puedan intercambiar los elementos de la lista nume
+                lista[j + 1] = list;
+            }
+        }
+    }
+    return lista;
+}
+
+const nume = [5, 54, 100, 1, 12, 71, 10, 8, 2, 7];
+console.log('La lista ordenada es', listan(nume));
+
+// 8. Eliminar elementos duplicados: Dada una lista de elementos, crea una función en JavaScript que recorra la lista y elimine los elementos duplicados. Devuelve una nueva lista que contenga únicamente los elementos únicos.
+
+
+
+// 9. Suma de dos listas: Dadas dos listas de números del mismo tamaño, crea una función en JavaScript que recorra ambas listas y devuelva una nueva lista donde cada elemento sea la suma de los elementos correspondientes de las dos listas.
+
+
+
+// 10. Encontrar la intersección de dos listas: Dadas dos listas de números, crea una función en JavaScript que recorra ambas listas y devuelva una nueva lista que contenga los elementos comunes a ambas listas.
